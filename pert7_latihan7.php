c9b9780db71351e8bf25ca0ea1f059502d830e6b
sigit maulana <79891404+maulanasigit20@users.noreply.github.com>
2021-03-17 23:14:43 +0700

    latihan pertemuan 7

--- pert7_latihan7.php
+++ pert7_latihan7.php
@@ -4,7 +4,7 @@
 </head> 
 <body> 
 <?php 
-	//info dari php.net  : This function was DEPRECATED in PHP 5.3.0, and REMOVED in PHP 7.0.0. | karena split dihapus di PHP7 maka kita ganti dengan explode()
+	//info dari php.net  : This function was DEPRECATED in PHP 5.3.0, and REMOVED in PHP 7.0.0. || karena split dihapus di PHP7 maka kita ganti dengan explode()
 	
 	$tanggal = "17-05-2010"; 
 	list($hari, $bulan, $tahun) = explode("-", $tanggal); 
