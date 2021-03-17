faf06b74f90fc02480ec3845637349c13190b3c5
sigit maulana <79891404+maulanasigit20@users.noreply.github.com>
2021-03-17 22:35:21 +0700

    latihan pertemuan 9

---
+++ pert9_latihan4.php
@@ -0,0 +1,14 @@
+<html> 
+<head> 
+<title> Tanggal </title> 
+</head> 
+<body> 
+<font size="10px">
+<?php 
+	echo "Sekarang tanggal "; 
+	echo date('d-F-Y'); 
+	echo "<br>dan jam "; 
+	echo date('h:i:s A'); 
+?> 
+</body> 
+</html> 
