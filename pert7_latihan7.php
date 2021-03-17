ede10fdd4b2ee972bb1f406782e705d97e40e2d7
sigit maulana <79891404+maulanasigit20@users.noreply.github.com>
2021-03-17 23:12:59 +0700

    update split to explode
    Info dari php.net  : This function was DEPRECATED in PHP 5.3.0, and REMOVED in PHP 7.0.0.
    karena split dihapus di PHP7 maka diganti dengan explode()

--- pert7_latihan7.php
+++ pert7_latihan7.php
@@ -4,8 +4,10 @@
 </head> 
 <body> 
 <?php 
+	//info dari php.net  : This function was DEPRECATED in PHP 5.3.0, and REMOVED in PHP 7.0.0. | karena split dihapus di PHP7 maka kita ganti dengan explode()
+	
 	$tanggal = "17-05-2010"; 
-	list($hari, $bulan, $tahun) = split("-", $tanggal); 
+	list($hari, $bulan, $tahun) = explode("-", $tanggal); 
 	echo "Hari = $hari"; 
 	echo "<br />"; 
 	echo "Bulan = $bulan"; 
